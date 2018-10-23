3d2528ec6c5877242200a6d5e85ea4a7ab3f5bd8
Ana Harris <vana1990@gmail.com>
2018-10-23 16:18:06 -0400

    added Gallery.search_by_floor method

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,4 +2,12 @@ class Gallery < ActiveRecord::Base
   has_many :art_pieces
   has_many :artists, through: :art_pieces
   has_many :periods, through: :art_pieces
+
+  def self.search_by_floor(arg)
+    galleries = self.all.select {|gal| gal.floor == arg}
+    rooms = galleries.collect {|el| el.theme}.join(", ")
+    puts "Floor #{arg}: #{rooms}"
+  end
+
+
 end
