32522af082ed4f4003bf93520a9d715403ab364f
Ana Harris <vana1990@gmail.com>
2018-10-23 11:31:27 -0400

    Console works, seed data in the db, ready to be tested

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,4 +2,6 @@ class Artist < ActiveRecord::Base
   has_many :art_pieces
   has_many :galleries, through: :art_pieces
   has_many :periods, through: :art_pieces
+
+  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,10 @@
 require_relative '../config/environment.rb'
+require 'pry'
+
+Artist.delete_all
+Period.delete_all
+ArtPiece.delete_all
+Gallery.delete_all
 
 dali = Artist.create(name: "Salvador Dali", culture: "Spanish", life_span: "1904 - 1989")
 picasso = Artist.create(name: "Pablo Picasso", culture: "Spanish", life_span: "1881 - 1973")
@@ -10,11 +16,10 @@ cubism = Period.create(name: "Cubism")
 dga = Period.create(name: "Dutch Golden Age")
 contemporary = Period.create(name: "Contemporary Art")
 
-
-night_watch = ArtPiece.create(name: "The Night Watch", classification: "Oil on canvas", period_id: 3, artist_id: 3, gallery_id: 1)
-ww = ArtPiece.create(name: "The Weeping Woman", classification: "Oil on canvas", period_id: 2, artist_id: 2, gallery_id: 2)
-memory = ArtPiece.create(name: "The Persistence of Memory", classification: "Oil on canvas", period_id: 1, artist_id: 1, gallery_id: 2)
-perro = ArtPiece.create(name: "Perro", classification: "Sculpture", period_id: 4, artist_id: 4, gallery_id: 3)
+night_watch = ArtPiece.create(name: "The Night Watch", classification: "Oil on canvas", period_id: dga.id, artist_id: rembrandt.id, gallery_id: gal1.id)
+ww = ArtPiece.create(name: "The Weeping Woman", classification: "Oil on canvas", period_id: cubism.id, artist_id: picasso.id, gallery_id: gal2.id)
+memory = ArtPiece.create(name: "The Persistence of Memory", classification: "Oil on canvas", period_id: surrealism.id, artist_id: dali.id, gallery_id: gal2.id)
+perro = ArtPiece.create(name: "Perro", classification: "Sculpture", period_id: contemporary.id, artist_id: botero.id, gallery_id: gal3.id)
 
 gal1 = Gallery.create(name: "Dutch painters", floor: 1, theme: "Masters of Dutch 17th century art")
 gal2 = Gallery.create(name: "Spanish painters", floor: 3, theme: "Spanish painters from the XX century")
