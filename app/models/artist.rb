abbb6c66b2607a705bcbdc337ea99efc308a5ac0
Ana Harris <vana1990@gmail.com>
2018-10-23 12:05:13 -0400

    added method Artist.most_common_culture

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,15 +3,22 @@ class Artist < ActiveRecord::Base
   has_many :galleries, through: :art_pieces
   has_many :periods, through: :art_pieces
 
-  def works #lists the names of all pieces of art for an artist
+  #lists the names of all pieces of art for an artist
+  def works
     self.art_pieces.map { |piece| piece.name }
   end
 
-  def artwork_locations #lists all pieces of art and the corresponding gallery
+  #lists all pieces of art and the corresponding gallery
+  def artwork_locations
     self.art_pieces.collect do |piece|
       puts "#{piece.name} is located at the #{piece.gallery.name} gallery, floor #{piece.gallery.floor}."
     end
   end
 
+  #return the name of the most common culture among artists
+  def self.most_common_culture
+    artist = Artist.all.max_by {|artist| artist.culture}
+    artist.culture
+  end
 
 end
