86008cf3c7af3409bdc27edd34f45ef8cc6429c5
Ana Harris <vana1990@gmail.com>
2018-10-23 11:51:22 -0400

    Built first two method: .works and .artwork_locations for Artist class

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,5 +3,15 @@ class Artist < ActiveRecord::Base
   has_many :galleries, through: :art_pieces
   has_many :periods, through: :art_pieces
 
-  
+  def works #lists the names of all pieces of art for an artist
+    self.art_pieces.map { |piece| piece.name }
+  end
+
+  def artwork_locations #lists all pieces of art and the corresponding gallery
+    self.art_pieces.collect do |piece|
+      puts "#{piece.name} is located at the #{piece.gallery.name} gallery, floor #{piece.gallery.floor}."
+    end
+  end
+
+
 end
