20874e48a203e52e97182fa9a513f6c10592010e
Drewisatlas <drewisatlas@gmail.com>
2018-10-25 13:20:39 -0400

    built out cli to main search groups

--- app/models/cli.rb
+++ app/models/cli.rb
@@ -1,19 +1,56 @@
 
-  def start
-    puts "==========  Welcome to the Flatiron Museum of Art!  =========="
+  def start #first
+    puts "==========  Welcome to the Flatiron Museum of Art! //  =========="
     puts "Enter 'menu' to access the menu."
     puts "Enter 'q' to exit."
+
     input = gets.chomp
+
     case(input)
-    when 'menu'
-      menu
-    when 'q'
-      puts "See you next time!"
-      exit
+      when 'menu'
+        menu
+      when 'q'
+        exit_method
+      else
+        puts "Please enter 'menu' to continue or 'q' to exit"
     end
   end
 
-  def menu
-    puts ""
-
+  def exit_method
+    puts "See you next time!"
+    exit
   end
+
+  def menu
+    puts "What are you looking for?"
+    puts "1. Find by Artist"
+    puts "2. Find by Artwork"
+    puts "3. Gallery information"
+    puts "4. exit"
+    puts "Please enter a number:"
+
+    input = gets.chomp
+
+    case(input)
+      when '1'
+        artist_prompts
+      when '2'
+        artwork_prompts
+      when '3'
+        gallery_prompts
+      when '4'
+        exit_method
+    else
+      puts "Please enter a valid number"
+    end
+  end
+
+def artist_prompts
+  
+end
+
+def artwork_prompts
+end
+
+def gallery_prompts
+end
